0d3c2b3c6329bf002d7a13998135cb5f651992fd
nayoung <yny3533@naver.com>
2025-06-25 13:29:08 +0900

    [chore] initial commit

---
+++ src/main/java/com/example/shimpyo/ShimPyoApplication.java
@@ -0,0 +1,13 @@
+package com.example.shimpyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShimPyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShimPyoApplication.class, args);
+    }
+
+}
