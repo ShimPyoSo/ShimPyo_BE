304a7ad76bb18de8343d76734dab64d9d95c63d6
nayoung <yny3533@naver.com>
2025-06-25 13:36:40 +0900

    [chore] initial commit

---
+++ src/test/java/com/example/shimpyo/ShimPyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shimpyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShimPyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
