668f7b1650a0e8113058627e200cb9948bc49f22
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2025-07-05 14:51:52 +0900

    fix : 불필요한 try-catch 제거
    Global Exception 으로 관리함

--- src/main/java/com/example/shimpyo/domain/user/controller/UserController.java
+++ src/main/java/com/example/shimpyo/domain/user/controller/UserController.java
@@ -20,24 +20,15 @@ public class UserController {
     // [#MOO1] 사용자 회원가입 시작
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody RegisterUserRequest dto){
-        try {
-            authService.registerUser(dto);
-            return ResponseEntity.ok("회원가입 완료");
-        }catch (BaseException e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-
+        authService.registerUser(dto);
+        return ResponseEntity.ok("회원가입 완료");
     }
     // [#MOO1] 사용자 회원가입 끝
 
     // [#M002] 이메일 검증 시작
     @GetMapping("/check/email/duplicate")
     public ResponseEntity<?> getEmail(@RequestParam String email){
-        try{
-            return ResponseEntity.ok(authService.emailCheck(email));
-        }catch (BaseException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
-        }
+        return ResponseEntity.ok(authService.emailCheck(email));
     }
     // [#M002] 이메일 검증 끝
 }
