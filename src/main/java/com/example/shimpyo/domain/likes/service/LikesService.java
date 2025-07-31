8ae5f3c1b610d67bf8179f328925d52f4b12c5ba
nayoung <yny3533@naver.com>
2025-07-31 15:32:37 +0900

    [feat] 마이페지이 관광지 찜 목록 기능 구현

--- src/main/java/com/example/shimpyo/domain/likes/service/LikesService.java
+++ src/main/java/com/example/shimpyo/domain/likes/service/LikesService.java
@@ -2,15 +2,21 @@ package com.example.shimpyo.domain.likes.service;
 
 import com.example.shimpyo.domain.auth.service.AuthService;
 import com.example.shimpyo.domain.likes.controller.repository.LikesRepository;
+import com.example.shimpyo.domain.tourist.entity.Category;
 import com.example.shimpyo.domain.tourist.entity.Tourist;
 import com.example.shimpyo.domain.tourist.service.TouristService;
+import com.example.shimpyo.domain.user.dto.TouristLikesResponseDto;
 import com.example.shimpyo.domain.user.entity.Likes;
 import com.example.shimpyo.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -30,5 +36,13 @@ public class LikesService {
         } else {
             user.getLikes().add(likesRepository.save(Likes.builder().user(user).tourist(tourist).build()));
         }
+    }
+
+    public List<TouristLikesResponseDto> getTouristLikes(String category, Long id) {
+        User user = authService.findUser().getUser();
+        Pageable pageable = PageRequest.of(0, 8);
+
+        return "all".equalsIgnoreCase(category)? likesRepository.findLikesDtoByUser(user, id, pageable):
+                likesRepository.findLikesDtoByUserAndCategory(user, id, Category.fromCode(category), pageable);
     }
 }
