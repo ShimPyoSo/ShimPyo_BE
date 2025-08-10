2b217a18d3bb78f0b24798a621115646fc44d168
nayoung <yny3533@naver.com>
2025-08-10 16:36:01 +0900

    [fix] Review 반환 시 List<String> images 컬럼에 의한 에러 수정

--- src/main/java/com/example/shimpyo/domain/user/entity/Review.java
+++ src/main/java/com/example/shimpyo/domain/user/entity/Review.java
@@ -2,6 +2,7 @@ package com.example.shimpyo.domain.user.entity;
 
 import com.example.shimpyo.domain.common.BaseEntity;
 import com.example.shimpyo.domain.tourist.entity.Tourist;
+import com.example.shimpyo.utils.StringListConverter;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,8 +30,8 @@ public class Review extends BaseEntity {
     @Column(nullable = false)
     private String content;
 
-    @Column
-    // 이미지 여러개 넣는 경우가 있을 수 있으니 List 로 설정함
+    @Column(length = 3000)
+    @Convert(converter = StringListConverter.class)
     private List<String> image = new ArrayList<>();
 
     @ManyToOne
