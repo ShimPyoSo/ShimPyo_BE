b090775c26bb8c052508e99efc44b55272cf774f
nayoung <yny3533@naver.com>
2025-08-10 15:22:43 +0900

    [fix] ReviewRequestDto 검증 오류 수정

--- src/main/java/com/example/shimpyo/domain/tourist/dto/ReviewRequestDto.java
+++ src/main/java/com/example/shimpyo/domain/tourist/dto/ReviewRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.shimpyo.domain.tourist.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,7 +10,7 @@ import java.util.List;
 @Getter
 @Builder
 public class ReviewRequestDto {
-    @NotBlank(message = "id 오류입니다.")
+    @NotNull(message = "id 오류입니다.")
     private Long id;
     @NotBlank(message = "5글자 이상 입력하세요.")
     private String contents;
