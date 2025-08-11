34fb9ddfdde3a94fddbcc4c56ff7d2891ba3136d
nayoung <yny3533@naver.com>
2025-08-11 15:10:25 +0900

    [fix] 누락된 파일 추가

--- src/main/java/com/example/shimpyo/domain/user/repository/ReviewRepository.java
+++ src/main/java/com/example/shimpyo/domain/user/repository/ReviewRepository.java
@@ -2,9 +2,12 @@ package com.example.shimpyo.domain.user.repository;
 
 import com.example.shimpyo.domain.tourist.entity.Tourist;
 import com.example.shimpyo.domain.user.dto.MyReviewListResponseDto;
+import com.example.shimpyo.domain.user.dto.MyReviewListResponseDto;
 import com.example.shimpyo.domain.user.entity.Review;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -18,11 +21,11 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     List<Review> findByTouristOrderByIdDesc(Tourist tourist, Pageable pageable);
 
+
     @Query("SELECT new com.example.shimpyo.domain.user.dto.MyReviewListResponseDto(" +
             "r.tourist.id, r.tourist.region, r.tourist.name, r.tourist.image, r.tourist.address, COUNT(r)) " +
             "FROM Review r " +
             "WHERE r.user.id = :userId " +
             "GROUP BY r.tourist.id, r.tourist.region, r.tourist.name, r.tourist.image, r.tourist.address")
     List<MyReviewListResponseDto> countReviewsByTouristForUser(@Param("userId") Long userId);
-
 }
