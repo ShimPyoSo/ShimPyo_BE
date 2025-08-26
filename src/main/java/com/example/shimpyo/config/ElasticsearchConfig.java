5a1c82409b80b7374323920ac48bfbfcf5a513c8
nayoung <yny3533@naver.com>
2025-08-26 21:54:54 +0900

    [chore] elastic url 수정

--- src/main/java/com/example/shimpyo/config/ElasticsearchConfig.java
+++ src/main/java/com/example/shimpyo/config/ElasticsearchConfig.java
@@ -28,7 +28,7 @@ public class ElasticsearchConfig extends ElasticsearchConfiguration {
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-                .connectedTo(elasticsearchUrl)
+                .connectedTo(elasticsearchUrl.replace("http://", ""))
                 .build();
     }
 }
