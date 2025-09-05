5819b2c2271dceaf83ad760217b353a78effab43
nayoung <yny3533@naver.com>
2025-09-05 13:54:23 +0900

    [fix] JWT filter 로직 수정

--- src/main/java/com/example/shimpyo/domain/auth/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/shimpyo/domain/auth/jwt/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package com.example.shimpyo.domain.auth.jwt;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
@@ -14,8 +15,26 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        if (extractRefreshToken(request) == null)
+            setResponse(response, "접근 권한이 없습니다.");
+        else
+            setResponse(response, "만료된 토큰입니다.");
+
+    }
+
+    private String extractRefreshToken(HttpServletRequest request) {
+        if (request.getCookies() == null) return null;
+        for (Cookie cookie : request.getCookies()) {
+            if ("refresh_token".equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+
+    private void setResponse(HttpServletResponse response, String messsage) throws IOException {
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(String.format("{\"message\": \"%s\"}", "접근 권한이 없습니다."));
+        response.getWriter().write(String.format("{\"message\": \"%s\"}", messsage));
     }
 }
